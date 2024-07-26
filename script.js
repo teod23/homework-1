59f7128ae0e9ddc568dc61ddc52cd1a6200e960a
teod23 <168969458+teod23@users.noreply.github.com>
2024-07-26 13:10:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Name is : Teona");
+console.log("Surname is : Davitadze");
+console.log("Age : 22");
+console.warn("Email : Test@gmail.com")
+console.warn("Password : 123456")
+console.log("Phone : 555-555-555");
